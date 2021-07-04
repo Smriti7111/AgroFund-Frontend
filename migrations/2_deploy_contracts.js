3cd3d98bfc35fdade4d3f766a7d166fcca854a8d
Smriti <smritibanjade71@gmail.com>
2021-07-04 15:14:14 +0545

    Add deploy code for the investor and farmer contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var FarmerFactory = artifacts.require("./FarmerFactory.sol");
+var InvestorFactory = artifacts.require("./InvestorFactory.sol");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(FarmerFactory);
+  deployer.deploy(InvestorFactory);
 };
