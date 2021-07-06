991b6237fd129997506b2e2f9d470c0711fa26ae
Smriti <smritibanjade71@gmail.com>
2021-07-06 13:25:50 +0545

    Folder structure for pages and components/add initial file for login and signup

---
+++ client/src/pages/login/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return "Sign Up page";
+};
+
+export default Login;
---
+++ client/src/pages/signup/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return "Sign Up page";
+};
+
+export default SignUp;
