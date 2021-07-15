c83e22f8631cb7e9179eed971fa63a905244d9e3
subedisagar12 <subedisagar12@gmail.com>
2021-07-15 19:22:29 +0545

    Commit to merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { useContext, useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 import "./App.css";
 import { walletContext } from "./Context/WalletContext";
