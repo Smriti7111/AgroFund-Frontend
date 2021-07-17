e7772af9cf551eb694062e8c89f5298c8b83c819
Smriti <smritibanjade71@gmail.com>
2021-07-17 23:26:32 +0545

    Add condition for verify / create a project / view my project

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,6 +7,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { useHistory } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,11 +24,22 @@ const useStyles = makeStyles((theme) => ({
 const Navbar = () => {
   const classes = useStyles();
   const history = useHistory();
+  const [cookies, setCookie] = useCookies(["user"]);
 
   const handleLogout = () => {
     sessionStorage.setItem("token", "");
     sessionStorage.clear();
     history.push("/login");
+  };
+
+  const showButton = () => {
+    if (cookies.User.hasProject) {
+      return <Button color="inherit">View my Project</Button>;
+    } else if (!cookies.User.hasProject && cookies.User.isVerified) {
+      return <Button color="inherit">Create a Project</Button>;
+    } else {
+      return <Button color="inherit">Verify</Button>;
+    }
   };
 
   return (
@@ -45,6 +57,7 @@ const Navbar = () => {
           <Typography variant="h6" className={classes.title}>
             Agro Fund
           </Typography>
+          {showButton()}
           <Button onClick={handleLogout} color="inherit">
             Logout
           </Button>
