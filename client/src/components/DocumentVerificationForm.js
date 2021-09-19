6edf03d8da1c0cf15b84d1147c7b7c305686d670
Smriti <smritibanjade71@gmail.com>
2021-09-19 14:00:20 +0545

    clean code

--- client/src/components/DocumentVerificationForm.js
+++ client/src/components/DocumentVerificationForm.js
@@ -7,15 +7,6 @@ import axios from "axios";
 import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
   form: {
     width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
