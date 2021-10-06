3fa81eae3352deec87059a683029156298e08d80
Smriti <smritibanjade71@gmail.com>
2021-10-06 18:28:12 +0545

    Add card in the individual farmer and investor detail page

--- client/src/pages/dashboard/investor/individualInvestorDetail.js
+++ client/src/pages/dashboard/investor/individualInvestorDetail.js
@@ -1,7 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 500,
+    height: 500,
+  },
+  title: {
+    marginBottom: 12,
+  },
+  pos: {
+    fontSize: 14,
+  },
+});
 
 const IndividualInvestorDetail = () => {
-  return "Individual investor data";
+  const classes = useStyles();
+
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            >
+              Investor Details
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default IndividualInvestorDetail;
